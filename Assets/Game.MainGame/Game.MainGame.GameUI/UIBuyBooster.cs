669d3d6f886523df8db74e7bf4513949b5d769c2
huylephuc <92905979+huylephuc@users.noreply.github.com>
2025-03-11 18:03:49 +0700

    1

--- Assets/Game.MainGame/Game.MainGame.GameUI/UIBuyBooster.cs
+++ Assets/Game.MainGame/Game.MainGame.GameUI/UIBuyBooster.cs
@@ -1,21 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.MainGame
 {
     public class UIBuyBooster : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        [SerializeField] private Text _txtName;
+        [SerializeField] private Text _txtDescript;
+        [SerializeField] private Button _btnBuyCoin;
+        [SerializeField] private Button _btnBuyAds;
     }
 }
